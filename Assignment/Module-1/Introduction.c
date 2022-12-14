3979edb70c553f7c48e7199df8614003b4935f80
deep764 <120081895+deep764@users.noreply.github.com>
2022-12-14 14:51:10 +0530

    Add files via upload

---
+++ Assignment/Module-1/Introduction.c
@@ -0,0 +1,9 @@
+// WAP information 
+#include<stdio.h>
+void main()
+{
+	printf("\nDeep Khanpara");
+	printf("\n5 feb. 2003");
+	printf("\n19");
+	printf("\n\'DEEP'block No.37,\n Upasana park,\n Near Balaji hall,\n Rajkot,\n 360004");	
+}
