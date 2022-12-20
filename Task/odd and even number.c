2ff05666611482b61c68193f39d18d0f2da45a9f
deep764 <120081895+deep764@users.noreply.github.com>
2022-12-20 16:05:38 +0530

    Add files via upload

---
+++ Task/odd and even number.c
@@ -0,0 +1,16 @@
+//WAP Odd & Even number
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter the any number:");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf("even number");
+	}
+	else
+	{
+		printf("odd number");
+	}
+}
