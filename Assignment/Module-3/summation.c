31023af2e4edc8028e4c9770027fc6598ecdc746
deep764 <120081895+deep764@users.noreply.github.com>
2022-12-26 16:29:08 +0530

    Add files via upload

---
+++ Assignment/Module-3/summation.c
@@ -0,0 +1,18 @@
+//WAP summation
+#include<stdio.h>
+int main ()
+{
+	int sum,a,b,c,d;
+	printf("Enter the number\a:");
+	scanf("%d",&a);
+	printf("Enter the second number:");
+	scanf("%d",&b);
+	printf("Enter the third number:");
+	scanf("%d",&c);
+	printf("Enter the forth number:");
+	scanf("%d",&d);
+	sum=a+b+c+d;
+	printf("summation is:%d",sum);
+	return 0;
+}
+
