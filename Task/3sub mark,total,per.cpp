a708490a623bc61efb45419f128b9165ea411a64
deep764 <120081895+deep764@users.noreply.github.com>
2023-01-11 13:22:52 +0530

    Add files via upload

---
+++ Task/3sub mark,total,per.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+	int phy,maths,che,total;
+	float per;
+	printf("Physics marks:");
+	scanf("%d",&phy);
+	printf("maths marks:");
+	scanf("%d",&maths);
+	printf("chemistry marks:");
+	scanf("%d",&che);
+	total=phy+maths+che;
+	per=total/3;
+	
+	if(phy>=33&&maths>=33&&che>=33)
+	{
+		printf("\nTotal marks:%d",total);
+		printf("\nTotal percentage:%f",per);
+		if(per>75) 
+		{
+			printf("\nDistinction");
+		}
+		else if((per>60) && (per<=75))
+		{
+			printf("\nFirst class");
+		}
+		else if((per>50) && (per<=60))
+		{
+			printf("\nSecond class");
+		}
+		else if((per>35) && (per<=50))
+		{
+			printf("\nPass class");
+		}
+	}
+	else	
+	{
+		printf("Fail");
+	}
+	return 0;
+}
+
