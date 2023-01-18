ee443c04b0f7deedb43a9b5a40f34267ce876a1c
deep764 <120081895+deep764@users.noreply.github.com>
2023-01-18 13:56:04 +0530

    Add files via upload

---
+++ Task/age increment,decrement.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int age;
+	printf("Enter your age:");
+	scanf("%d",&age);
+	age++;
+	printf("\nAfter Increment age:%d",age);
+	age--;
+	age--;
+ 	printf("\nAfter Decrement age:%d",age);
+	return 0;
+}
