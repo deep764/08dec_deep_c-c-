a9cee66a85776fd89e5cbce197aa804b336964aa
deep764 <120081895+deep764@users.noreply.github.com>
2023-01-18 10:44:16 +0530

    Add files via upload

---
+++ Task/Decrement.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int age;
+	printf("Enter your age:");
+	scanf("%d",&age);
+	age--;
+	printf("\nAfter Decrement age:%d",age);
+	return 0;
+}
---
+++ Task/Increment.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int age;
+	printf("Enter your age:");
+	scanf("%d",&age);
+	age++;
+	printf("\nAfter increment age:%d",age);
+	return 0;
+}
